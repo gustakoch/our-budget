e6d63f774939494a257573401ef855ab3ad926f8
Gustavo Koch <gustavok@outlook.com.br>
2021-09-01 01:01:40 -0300

    Add favicon on login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="_token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('/images/budget-logo.png') }}" />
 
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
