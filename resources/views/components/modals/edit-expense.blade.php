d817acd5143111599dfdd38258c2de96a0429b80
Gustavo Koch <gustavok@outlook.com.br>
2021-09-11 03:28:51 -0300

    ajuste modal despesas

--- resources/views/components/modals/edit-expense.blade.php
+++ resources/views/components/modals/edit-expense.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="editExpenseModal" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="editExpenseModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header border-bottom-0" id="modalHeaderEditExpense">
