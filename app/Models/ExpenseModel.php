64cecbed9f84136f2d8b583a82525b6519ce8499
Gustavo Koch <gustavok@outlook.com.br>
2023-07-18 23:31:19 -0300

    Debug pagina manutencao

--- app/Models/ExpenseModel.php
+++ app/Models/ExpenseModel.php
@@ -237,30 +237,5 @@ class ExpenseModel extends Model
             ->get();
 
         return $expenses;
-
-        $expenses2 = DB::select("
-            SELECT e.id
-                , e.description
-                , e.category
-                , e.installments
-                , e.installment
-                , e.month
-                , e.year
-                , e.budgeted_amount
-                , e.realized_amount
-                , (e.budgeted_amount - e.realized_amount) pending_amount
-                , c.description as category_description
-                , m.description as month_description
-                , u.nickname as user_name
-            FROM expenses e
-                , categories c
-                , months m
-                , users u
-            WHERE e.category = c.id
-            AND e.month = m.id
-            AND e.user_id = u.id
-            AND e.id = ?", [$id]);
-
-        return $expenses;
     }
 }
